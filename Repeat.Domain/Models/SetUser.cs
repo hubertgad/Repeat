22f2208bd52ec5bf2860ec8e90d27cbe6e3d479e
Hubert Gad <50676544+hubertgad@users.noreply.github.com>
2020-03-05 11:33:59 +0100

    Update SetUser.cs

--- Repeat.Domain/Models/SetUser.cs
+++ Repeat.Domain/Models/SetUser.cs
@@ -4,12 +4,9 @@ namespace Repeat.Models
 {
     public class SetUser
     {
-        //[Required]
         public int SetID { get; set; }
-        public Set Set { get; set; }
-        
-        //[Required]
         public string UserID { get; set; }
-        //public IdentityUser User { get; set; }
+        
+        public Set Set { get; set; }
     }
 }
