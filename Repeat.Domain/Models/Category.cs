d383e6a777061a694c118b998a967b0f99876837
Hubert Gad <50676544+hubertgad@users.noreply.github.com>
2020-03-05 11:37:37 +0100

    Update Category.cs

--- Repeat.Domain/Models/Category.cs
+++ Repeat.Domain/Models/Category.cs
@@ -6,11 +6,11 @@ namespace Repeat.Models
     public class Category
     {
         public int ID { get; set; }
-        [Required]
-        [MaxLength(50)]
+        [Required, MaxLength(50)]
         public string Name { get; set; }
         [Required]
         public string OwnerID { get; set; }
+        
         public ICollection<Question> Questions { get; set; }
     }
 }
