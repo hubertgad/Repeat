7d1096c88508faa4a257ba5b8ab7f0f8285b214f
Hubert Gad <50676544+hubertgad@users.noreply.github.com>
2020-03-05 11:38:46 +0100

    Update Answer.cs

--- Repeat.Domain/Models/Answer.cs
+++ Repeat.Domain/Models/Answer.cs
@@ -5,13 +5,12 @@ namespace Repeat.Models
     public class Answer
     {
         public int ID { get; set; }
-        [Required]
         public int QuestionID { get; set; }
-        [Required]
-        [MaxLength(1000)]
+        [Required, MaxLength(1000)]
         public string AnswerText { get; set; }
         [Required]
         public bool IsTrue { get; set; }
+        
         public Question Question { get; set; }
 
         public Answer()
