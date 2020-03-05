32ab0a76ac772f1aff21e547eee992183c42e04a
Hubert Gad <50676544+hubertgad@users.noreply.github.com>
2020-03-05 11:33:17 +0100

    Update QuestionSet.cs

--- Repeat.Domain/Models/QuestionSet.cs
+++ Repeat.Domain/Models/QuestionSet.cs
@@ -5,9 +5,9 @@ namespace Repeat.Models
     public class QuestionSet
     {
         public int QuestionID { get; set; }
-        public Question Question { get; set; }
-
         public int SetID { get; set; }
+        
+        public Question Question { get; set; }
         public Set Set { get; set; }
     }
 }
