fca5db29be0b20a3749aeef9df27fd6a22721f63
Hubert Gad <hubertgad@gmail.com>
2020-03-07 14:47:45 +0100

    added data access layer

---
+++ Repeat.DataAccess/Migrations/20200305073402_Update23.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Repeat.Data.Migrations
+{
+    public partial class Update23 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SetUsers_AspNetUsers_UserID",
+                table: "SetUsers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_SetUsers_UserID",
+                table: "SetUsers");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAlive",
+                table: "Questions",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsAlive",
+                table: "Questions");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SetUsers_UserID",
+                table: "SetUsers",
+                column: "UserID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SetUsers_AspNetUsers_UserID",
+                table: "SetUsers",
+                column: "UserID",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ Repeat.DataAccess/Migrations/20200307120837_Update24.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Repeat.Data.Migrations
+{
+    public partial class Update24 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsAlive",
+                table: "Questions");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Questions",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Questions");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAlive",
+                table: "Questions",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
