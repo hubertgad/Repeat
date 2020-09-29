d27ec494f82e0744bcd39e97ab43180dd82edd3c
Hubert Gad <50676544+hubertgad@users.noreply.github.com>
2020-09-29 21:46:15 +0200

    Create SetServiceTests.cs

---
+++ Repeat.DataAccess.Tests/Services/SetServiceTests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using Repeat.DataAccess.Services;
+using Repeat.Domain.Interfaces;
+using Repeat.Domain.Models;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Repeat.DataAccess.Tests.Services
+{
+    [TestFixture]
+    class SetServiceTests : RepeatTestBase
+    {
+        private ISetService _setService;
+
+        [SetUp]
+        public override void SetUp()
+        {
+            base.SetUp();
+
+            _setService = new SetService(_context, _userService);
+        }
+
+        [Test]
+        public async Task AddSetAsync_WhenCalled_ShouldSaveShareInDb()
+        {
+            var set = new Set { ID = 3 };
+
+            await _setService.AddSetAsync(set);
+
+            var savedSet = _context.Sets.First(q => q.ID == set.ID);
+            Assert.That(savedSet, Is.EqualTo(set));
+        }
+
+        [Test]
+        public async Task AddSetAsync_NotValidOwnerId_ShouldCorrectId()
+        {
+            var set = new Set { ID = 3, OwnerID = "SecondUser" };
+
+            await _setService.AddSetAsync(set);
+
+            var savedSet = _context.Sets.First(q => q.ID == set.ID);
+            Assert.That(savedSet.OwnerID, Is.EqualTo(_userService.UserId));
+        }
+    }
+}
