6e0b18a118683aaddf82d3219ce9daf0395bb0be
Hubert Gad <50676544+hubertgad@users.noreply.github.com>
2020-09-30 20:37:56 +0200

    rename _currentUserService, update SeedTestDb()

--- Repeat.DataAccess.Tests/Services/SetServiceTests.cs
+++ Repeat.DataAccess.Tests/Services/SetServiceTests.cs
@@ -17,11 +17,11 @@ namespace Repeat.DataAccess.Tests.Services
         {
             base.SetUp();
 
-            _setService = new SetService(_context, _userService);
+            _setService = new SetService(_context, _currentUserService);
         }
 
         [Test]
-        public async Task AddSetAsync_WhenCalled_ShouldSaveShareInDb()
+        public async Task AddSetAsync_WhenCalled_ShouldSaveSetInDb()
         {
             var set = new Set { ID = 3 };
 
@@ -34,12 +34,88 @@ namespace Repeat.DataAccess.Tests.Services
         [Test]
         public async Task AddSetAsync_NotValidOwnerId_ShouldCorrectId()
         {
-            var set = new Set { ID = 3, OwnerID = "SecondUser" };
+            var set = new Set { ID = 3, OwnerID = "SecondUserId" };
 
             await _setService.AddSetAsync(set);
 
             var savedSet = _context.Sets.First(q => q.ID == set.ID);
-            Assert.That(savedSet.OwnerID, Is.EqualTo(_userService.UserId));
+            Assert.That(savedSet.OwnerID, Is.EqualTo(_currentUserService.UserId));
         }
+
+        [Test]
+        public async Task AddShareAsync_WhenCalled_ShouldSaveShareInDb()
+        {
+            await _setService.AddShareAsync(2, "Second User");
+
+            var savedShare = _context.Shares
+                .First(q => q.SetID == 2 && q.UserID == "SecondUserId");
+            Assert.That(savedShare, Is.Not.Null);
+        }
+
+        [Test]
+        public async Task AddShareAsync_WrongUserName_ShouldNotSaveShare()
+        {
+            await _setService.AddShareAsync(2, "Wrong Name");
+
+            var savedShare = _context.Shares.FirstOrDefault(q => q.SetID == 2);
+            Assert.That(savedShare, Is.Null);
+        }
+
+        [Test]
+        public async Task AddShareAsync_CurrentUserIsNotSetOwner_ShouldNotSaveShare()
+        {
+            _setUpContext.Sets.Add(new Set { ID = 3, OwnerID = "SecondUserId" });
+            _setUpContext.SaveChanges();
+
+            await _setService.AddShareAsync(3, "Third User");
+
+            var savedShare = _context.Shares.FirstOrDefault(q => q.SetID == 3);
+            Assert.That(savedShare, Is.Null);
+        }
+
+        [Test]
+        public async Task AddShareAsync_TryToShareToOwner_ShouldNotSaveShare()
+        {
+            await _setService.AddShareAsync(2, "User");
+
+            var savedShare = _context.Shares.FirstOrDefault(q => q.SetID == 2);
+            Assert.That(savedShare, Is.Null);
+        }
+
+        [Test]
+        public async Task RemoveSetAsync_WhenCalled_ShouldRemoveSetFromDb()
+        {
+            var set = _setUpContext.Sets.Find(1);
+
+            await _setService.RemoveSetAsync(set);
+
+            var removedSet = _context.Sets.FirstOrDefault(q => q.ID == 1);
+            Assert.That(removedSet, Is.Null);
+        }
+
+        [Test]
+        public async Task RemoveSetAsync_WhenCalled_ShouldRemoveAssociatedTests()
+        {
+            var set = _setUpContext.Sets.Find(1);
+
+            await _setService.RemoveSetAsync(set);
+
+            var removedTests = _context.Tests.Where(q => q.SetID == 1).ToList();
+            Assert.That(removedTests, Is.Empty);
+        }
+
+        [Test]
+        public async Task RemoveQuestionSetAsync()
+        {
+            var questionSet = _setUpContext.QuestionSets.Find(1, 1);
+
+            await _setService.RemoveQuestionFromSetAsync(questionSet);
+
+            var removedQuestionSet = _context.QuestionSets
+                .FirstOrDefault(q => q.QuestionID == 1 && q.SetID == 1);
+            Assert.That(removedQuestionSet, Is.Null);
+        }
+
+        // ...
     }
 }
