ad483d254381498d52c0dc86669a72604e9771f4
Hubert Gad <50676544+hubertgad@users.noreply.github.com>
2020-10-04 20:24:28 +0200

    js script exported to separate file

---
+++ Repeat/wwwroot/js/tab.js
@@ -0,0 +1,13 @@
+﻿$('body').on('keydown', 'textarea', function (e) {
+    if (e.which == 9) {
+        e.preventDefault();
+        var start = this.selectionStart;
+        var end = this.selectionEnd;
+
+        $(this).val($(this).val().substring(0, start)
+            + "\t"
+            + $(this).val().substring(end));
+
+        this.selectionStart = this.selectionEnd = start + 1;
+    }
+});
