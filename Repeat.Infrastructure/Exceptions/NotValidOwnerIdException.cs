2c17b98b34ce7793566b5dd684d231352224450f
Hubert Gad <50676544+hubertgad@users.noreply.github.com>
2020-10-04 17:30:01 +0200

    Create NotValidOwnerIdException.cs

---
+++ Repeat.Infrastructure/Exceptions/NotValidOwnerIdException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Repeat.Infrastucture.Exceptions
+{
+    public class NotValidOwnerIdException : Exception
+    {
+        public NotValidOwnerIdException()
+        {
+        }
+
+        public NotValidOwnerIdException(string message) : base(message)
+        {
+        }
+
+        public NotValidOwnerIdException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
