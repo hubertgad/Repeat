5c6bb5480a3bc588869edd3211b9147c1a56f0c8
Hubert Gad <50676544+hubertgad@users.noreply.github.com>
2020-10-06 19:43:25 +0200

    Create TestServiceTests.cs

---
+++ Repeat.Infrastructure.Tests/Services/TestServiceTests.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using Repeat.Domain.Interfaces;
+using Repeat.Infrastructure.Services;
+using Repeat.Infrastucture.Exceptions;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Repeat.Infrastructure.Tests.Services
+{
+    [TestFixture]
+    class TestServiceTests : RepeatTestBase
+    {
+        private ITestService _testService;
+
+        [SetUp]
+        public override void SetUp()
+        {
+            base.SetUp();
+
+            _testService = new TestService(_context, _currentUserService);
+        }
+
+        [Test]
+        public async Task CreateTestFromSetAsync_WhenCalled_ShouldSaveTestInDbAsync()
+        {
+            var initialTestCount = _setUpContext.Tests.Count();
+
+            await _testService.CreateTestFromSetAsync(1);
+
+            var currentTestCount = _context.Tests.Count();
+            Assert.That(currentTestCount, Is.GreaterThan(initialTestCount));
+        }
+
+        [Test]
+        public void CreateTestFromSetAsync_NotSharedToUser_ThrowAccessDeniedException()
+        {
+            Assert.That(async () => await _testService.CreateTestFromSetAsync(3),
+                Throws.Exception.TypeOf<AccessDeniedException>());
+        }
+
+    }
+}
