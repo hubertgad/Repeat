92247411bffc9db9a487e8b3764c389677f9904a
Hubert Gad <50676544+hubertgad@users.noreply.github.com>
2020-10-06 19:43:22 +0200

    Create AccessDeniedException.cs

---
+++ Repeat.Infrastructure/Exceptions/AccessDeniedException.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.Serialization;
+using System.Security.Permissions;
+
+namespace Repeat.Infrastucture.Exceptions
+{
+    [Serializable]
+    public class AccessDeniedException : Exception
+    {
+        public AccessDeniedException()
+        {
+        }
+
+        public AccessDeniedException(string message) : base(message)
+        {
+        }
+
+        public AccessDeniedException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected AccessDeniedException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+
+        [SecurityPermission(SecurityAction.Demand, SerializationFormatter = true)]
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            base.GetObjectData(info, context);
+        }
+    }
+}
